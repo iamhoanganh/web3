6e052732e6c9a1f734973d8460f860b5dd1143f8
Hoang Anh <iamnguyenhoanganh@outlook.com>
2023-06-17 16:28:15 +0700

    upload source

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,15 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
-    version: '0.8.9',
+    version: "0.8.9",
+    defaultNetwork: "sepolia",
+    networks: {
+      hardhat: {},
+      sepolia: {
+        url: "https://rpc.ankr.com/eth_sepolia/8ed2b36fbbcb173125fd76d847b026349d98d55e754473ded721b1163fba861c",
+        accounts: [`0x${process.env.PRIVATE_KEY}`],
+      },
+    },
     settings: {
       optimizer: {
         enabled: true,
